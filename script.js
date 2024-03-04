e16bf02232abfadec513f4aa49ea4f3378e0288d
unknown <Administrator@PC9-LAB4.labs.org>
2024-03-04 13:00:56 +0500

    non premitive and premitive data type

---
+++ script.js
@@ -0,0 +1,29 @@
+// var detail = [{
+//   name : "sameer ali",
+//   marks : [20,60,80,90,50],
+//   record : true
+// },
+// {
+//     name : "uzair",
+//     marks : [70,60,50,90,50],
+//     record : true
+// },
+// {
+//     name : "Aliyaan",
+//     marks : [20,60,50,75,60],
+//     record : false
+// },
+// {
+//     name : "Hamid rahna",
+//     marks : [20,60,80,45,60],
+//     record : false
+// },
+// {
+//     name : "abidr ali",
+//     marks : [20,60,80,90,50],
+//     record : true
+// }
+
+// ]
+
+// console.log(detail[0].marks[2]);
